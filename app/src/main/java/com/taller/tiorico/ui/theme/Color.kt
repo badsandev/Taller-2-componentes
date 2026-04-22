60b59f1320d7d84fe93e0fe901e7dcae1868d3ff
Alejocm11 <alejocatanomotta@gmail.com>
2026-04-21 21:49:01 -0500

    corrigiendo error

--- app/src/main/java/com/taller/tiorico/ui/theme/Color.kt
+++ app/src/main/java/com/taller/tiorico/ui/theme/Color.kt
@@ -1,2 +1,19 @@
 package com.taller.tiorico.ui.theme
 
+import androidx.compose.ui.graphics.Color
+
+val GoldPrimary = Color(0xFFFFD700)
+val GoldDark = Color(0xFFC5A300)
+val GoldLight = Color(0xFFFFE066)
+
+val GreenSuccess = Color(0xFF2ECC71)
+val RedExpense = Color(0xFFE74C3C)
+
+val DarkBackground = Color(0xFF121212)
+val DarkSurface = Color(0xFF1E1E1E)
+val LightBackground = Color(0xFFF8F9FA)
+val LightSurface = Color(0xFFFFFFFF)
+
+val TextPrimary = Color(0xFF2D3436)
+val TextSecondary = Color(0xFF636E72)
+val TextWhite = Color(0xFFFFFFFF)
